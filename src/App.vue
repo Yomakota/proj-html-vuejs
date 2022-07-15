f6558c3a1fe900090459f28d40dc2404c89ed3a8
Yomakota <89361386+Yomakota@users.noreply.github.com>
2022-07-15 11:49:14 +0200

    add firsts comments-bootstrap-assets

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,32 @@
+<!-- Creare un progetto con Vue CLI, strutturando il layout in almeno 3 macro-componenti:
+HeaderPage, MainPage e FooterPage.
+
+Suddivione FooterPage e HeaderPage in 2 micro-componenti
+Suddivione MainPage in 8 micro-componenti o section
+
+Popolare le voci di menù di HeaderPage e FooterPage dinamicamente, tramite delle props, creando una struttura dati idonea.
+Agginungere in App.vue i data che riporto con props alle componenti discendenti
+
+Preparo in assets le immagini e i colori a cui dare dei nomi piu specifici in base a quali componenti o sections verranno usati
+importo bootstrap -->
+
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import './assets/scss/style.scss';
 </style>
+
