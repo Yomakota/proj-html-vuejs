1687acf51b06b2c8d8f7a8090244437842631251
Yomakota <89361386+Yomakota@users.noreply.github.com>
2022-07-19 05:35:46 +0200

    add cooments

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,17 @@
 <!-- Creare un progetto con Vue CLI, strutturando il layout in almeno 3 macro-componenti:
 HeaderPage, MainPage e FooterPage.
 
-Suddivisione FooterPage in 2 e HeaderPage in 3 micro-componenti
-Suddivisione MainPage in 7/8 micro-componenti o sections
+Suddivisione FooterPage in 2 e HeaderPage in 3 micro-componenti + i componenti per menu dinamici
+Suddivisione MainPage in 7/8 micro-componenti o sections + componenti per menu dinamici
 
 Popolare le voci di menù di HeaderPage e FooterPage dinamicamente, tramite delle props, creando una struttura dati idonea.
+
 Aggiungere in App.vue i data che riporto con props alle componenti discendenti
 
-Preparo in assets le immagini e i colori a cui dare dei nomi più specifici in base a quali componenti o sections verranno usati
-importo bootstrap -->
+Preparo in assets le immagini e i colori e i file data
+
+importo bootstrap
+importo il font e fontawesome per icone -->
 
 <template>
   <div id="app">
